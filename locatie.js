34a1438e204ade90329441866df0bfb3f901e830
TicauStelian <102904077+TicauStelian02@users.noreply.github.com>
2023-10-17 14:37:50 +0300

    Update2 locatie.js

--- locatie.js
+++ locatie.js
@@ -1,12 +1,11 @@
-get_geo_position();
 function on_success(position){
-document.getElementById("latitude").innerHTML = "Latitude = " + position.coords.latitude;
+document.getElementById("latitude").innerHTML = "Latitude =" + position.coords.latitude;
 document.getElementById("longitude").innerHTML = "Longitude= " + position.coords.longitude;
 document.getElementById("acc").innerHTML = "Accuracy = "+position.coords.accuracy;
 document.getElementById("altitude").innerHTML = "Altitude = "+position.coords.altitude;
 var latlon = position.coords.latitude + "," + position.coords.longitude;
 var img_url =
-"https://maps.googleapis.com/maps/api/staticmap?center="+latlon+ "&zoom=14&size=400x300&key=AIzaSyBXzbG272Mfoi_9HhMtUkvdUSonlBteUPk"; 
+"https://maps.googleapis.com/maps/api/staticmap?center="+latlon + "&zoom=14&size=400x300&key=AIzaSyBXzbG272Mfoi_9HhMtUkvdUSonlBteUPk";
 document.getElementById("map").innerHTML = "<img src='"+img_url+"'>";
 }
 function on_error(e)
